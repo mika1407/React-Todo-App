582af049915da3f0a56c21427885dfcccbc70189
mika1407 <koneautomaatio@hotmail.com>
2020-02-19 14:46:03 +0200

    Updates

---
+++ precache-manifest.4c5265d1488e48281719e25c93ba5276.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "092f7c13378ad008f01f85289c62dbd3",
+    "url": "/reacteka/index.html"
+  },
+  {
+    "revision": "44e5f4da6c6d6773232d",
+    "url": "/reacteka/static/css/main.8d65813d.chunk.css"
+  },
+  {
+    "revision": "3ec94444227bdf7b9f3b",
+    "url": "/reacteka/static/js/2.7ca5bb83.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/reacteka/static/js/2.7ca5bb83.chunk.js.LICENSE"
+  },
+  {
+    "revision": "44e5f4da6c6d6773232d",
+    "url": "/reacteka/static/js/main.ba4037ec.chunk.js"
+  },
+  {
+    "revision": "d708c634b2ad8a49c524",
+    "url": "/reacteka/static/js/runtime-main.255cf96c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-eka/precache-manifest.a91beb095638e1d6f82c51a7ab06235d.js"
+  "/reacteka/precache-manifest.4c5265d1488e48281719e25c93ba5276.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-eka/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reacteka/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
